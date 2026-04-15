1f93f2a6c537e31206140c21cf04140f5f31ed7d
Canmi <t@canmi.icu>
2026-04-15 12:44:54 +0800

    chore: remove navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
 	import '../styles/app.css';
-	import Navbar from '$lib/components/navbar.svelte';
 	import Footer from '$lib/components/footer.svelte';
 
 	let { data, children } = $props();
 </script>
 
-<Navbar />
-<main class="flex min-h-[calc(100svh-var(--nav-h))] flex-col">
+<main class="flex min-h-svh flex-col">
 	{@render children()}
 </main>
 <Footer runtimeDays={data.runtimeDays} />
