53251f06be9f28f5cf0e741b68934a1c2d97b3b3
Canmi <t@canmi.icu>
2026-04-15 12:55:28 +0800

    feat: add canonical link in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import '../styles/app.css';
+	import { page } from '$app/state';
 	import Footer from '$lib/components/footer.svelte';
 
 	let { data, children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="canonical" href="{__PUBLIC_URL__}{page.url.pathname}" />
+</svelte:head>
 
 <main class="flex min-h-svh flex-col">
 	{@render children()}
